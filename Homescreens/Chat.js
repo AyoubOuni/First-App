14e563ce1076a7fa916d07b9e9ed5596aefa3690
Abderrazek Souguir <yomyfriendhh@gmail.com>
2023-12-08 23:42:55 +0100

    add typing

--- Homescreens/Chat.js
+++ Homescreens/Chat.js
@@ -1,27 +1,25 @@
 import React, { useEffect, useState } from 'react';
-import { View, StyleSheet, Text, TextInput, TouchableOpacity, FlatList } from 'react-native';
+import { View, StyleSheet, Text, TextInput, TouchableOpacity, FlatList, KeyboardAvoidingView, Platform } from 'react-native';
+import { useRoute, useNavigation } from '@react-navigation/native'; // Import useNavigation
 import firebase from '../Config/Index';
-import { useRoute } from '@react-navigation/native';
 
 const database = firebase.database();
 
 export default function Chat() {
   const route = useRoute();
+  const navigation = useNavigation(); // Initialize navigation
   const { currentId, id_user } = route.params;
   const [msg, setMsg] = useState('');
   const [messages, setMessages] = useState([]);
- 
-  
+  const [isTyping, setIsTyping] = useState(false);
+
   useEffect(() => {
     const messageRef = database.ref('msgS').orderByChild('time');
-
-    // Listen for new messages and maintain the order
     messageRef.on('child_added', (snapshot) => {
       const newMessage = snapshot.val();
-      setMessages(prevMessages => [...prevMessages, { ...newMessage, id: snapshot.key }]);
+      setMessages((prevMessages) => [...prevMessages, { ...newMessage, id: snapshot.key }]);
     });
 
-    // Clean up the listener when the component unmounts
     return () => {
       messageRef.off('child_added');
     };
@@ -29,115 +27,135 @@ export default function Chat() {
 
   const sendMessage = () => {
     const currentTime = new Date().toISOString();
-    const ref_msg = database.ref("msgS");
+    const ref_msg = database.ref('msgS');
     const key = ref_msg.push().key;
     ref_msg.child(key).set({
       msg: msg,
       sender: currentId,
       receiver: id_user,
       time: currentTime,
-      status:false
+      status: false,
     });
     setMsg('');
+  };
+
+  const handleTyping = (isFocused) => {
+    const currentUserTypingRef = database.ref(`conversation/${currentId}_${id_user}/isTyping`);
+    currentUserTypingRef.set(isFocused);
+
+    const otherUserTypingRef = database.ref(`conversation/${id_user}_${currentId}/isTyping`);
+    otherUserTypingRef.on('value', (snapshot) => {
+      const otherUserIsTyping = snapshot.val();
+      setIsTyping(otherUserIsTyping);
+    });
   };
 
   const renderMessage = ({ item }) => {
     const isCurrentUser = item.sender === currentId;
 
     return (
-      <View style={[styles.messageContainer, isCurrentUser ? styles.currentUserMessage : styles.otherUserMessage]}>
-      <Text>{item.msg}</Text>
-    </View>
+      <View
+        style={[styles.messageContainer, isCurrentUser ? styles.currentUserMessage : styles.otherUserMessage]}
+      >
+        <Text>{item.msg}</Text>
+      </View>
     );
   };
 
- 
-
   return (
     <View style={styles.container}>
-    <FlatList
-      data={messages}
-      renderItem={renderMessage}
-      keyExtractor={(item) => item.id}
-      // Remove inverted attribute to display messages at the bottom
-    />
-      <View style={styles.inputContainer}>
+      <FlatList
+        data={messages}
+        renderItem={renderMessage}
+        keyExtractor={(item) => item.id}
+        keyboardShouldPersistTaps="always"
+      />
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        style={styles.inputContainer}
+      >
+        {isTyping && <Text style={styles.typingIndicator}>{`${currentId} current user`}</Text>}
+        {isTyping && <Text style={styles.typingIndicator}>{`${id_user} is typing...`}</Text>}
+        <TouchableOpacity style={styles.backButton} onPress={() => navigation.goBack()}>
+          <Text style={styles.backButtonText}>Back</Text>
+        </TouchableOpacity>
         <TextInput
           value={msg}
-          onChangeText={setMsg}
+          onChangeText={(text) => setMsg(text)}
+          onFocus={() => handleTyping(true)}
+          onBlur={() => handleTyping(false)}
           placeholder="Type your message here"
           style={styles.inputField}
         />
-        <TouchableOpacity
-          style={styles.sendButton}
-          onPress={sendMessage}
-        >
+        <TouchableOpacity style={styles.sendButton} onPress={sendMessage}>
           <Text style={styles.sendButtonText}>Send</Text>
         </TouchableOpacity>
-      </View>
+      </KeyboardAvoidingView>
     </View>
   );
 }
-const styles = StyleSheet.create({
-  hidden: {
-      display: 'none', // Not a valid style property in React Native
-      // You can use other styles like this to hide the component
-      width: 0,
-      height: 0,
-      opacity: 0,
-      // Or set the visibility to 'hidden'
-      visibility: 'hidden',
-    },
-container: {
-  flex: 1,
-  backgroundColor: "#fff",
-  paddingHorizontal: 15,
-},
-messageContainer: {
-  padding: 10,
-  marginBottom: 10,
-  borderRadius: 8,
-  maxWidth: "80%",
-},
-currentUserMessage: {
-  alignSelf: "flex-end",
-  backgroundColor: "#DCF8C6",
-},
-otherUserMessage: {
-  alignSelf: "flex-start",
-  backgroundColor: "#E6E6E6",
-},
-inputContainer: {
-  flexDirection: "row",
-  alignItems: "center",
-  borderTopWidth: 1,
-  borderTopColor: "#ccc",
-  paddingVertical: 10,
-},avatar: {
-  width: 40,
-  height: 40,
-  borderRadius: 20,
-  marginRight: 8,
-},
-inputField: {
-  flex: 1,
-  paddingHorizontal: 10,
-  paddingVertical: 8,
-  borderWidth: 1,
-  borderColor: "#ccc",
-  borderRadius: 20,
-  marginRight: 10,
-},
-sendButton: {
-  paddingVertical: 8,
-  paddingHorizontal: 15,
-  backgroundColor: "#007BFF",
-  borderRadius: 20,
-},
-sendButtonText: {
-  color: "#fff",
-  fontWeight: "bold",
-},
-});
 
-// Styles remain unchanged
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#121212",
+    paddingHorizontal: 15,
+    paddingBottom: 10,
+  },
+  messageContainer: {
+    padding: 10,
+    marginBottom: 10,
+    borderRadius: 8,
+    maxWidth: "80%",
+  },
+  currentUserMessage: {
+    alignSelf: "flex-end",
+    backgroundColor: "#6B52AE",
+  },
+  otherUserMessage: {
+    alignSelf: "flex-start",
+    backgroundColor: "#FFFFFF",
+  },
+  inputContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    borderTopWidth: 1,
+    borderTopColor: "#ccc",
+    paddingVertical: 10,
+  },
+  backButton: {
+    paddingVertical: 12,
+    paddingHorizontal: 20,
+    backgroundColor: "#6B52AE",
+    borderRadius: 25,
+    marginRight: 10,
+  },
+  backButtonText: {
+    color: "#FFFFFF",
+    fontWeight: "bold",
+  },
+  inputField: {
+    flex: 1,
+    paddingHorizontal: 15,
+    paddingVertical: 12,
+    borderWidth: 1,
+    borderColor: "#6B52AE",
+    borderRadius: 25,
+    marginRight: 10,
+    color: "#FFFFFF",
+  },
+  sendButton: {
+    paddingVertical: 12,
+    paddingHorizontal: 20,
+    backgroundColor: "#6B52AE",
+    borderRadius: 25,
+  },
+  sendButtonText: {
+    color: "#FFFFFF",
+    fontWeight: "bold",
+  },
+  typingIndicator: {
+    fontStyle: 'italic',
+    color: '#6B52AE',
+  },
+});
